bbf3e0c9eadbe6c13b7d46e8a7aff5cf2da7be14
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-08-14 20:34:44 -0300

    chore: add basic boilerplate code

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log('Hi');
+import { Telegraf } from 'telegraf';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const bot = new Telegraf(process.env.TOKEN);
+
+bot.on('text', (ctx) => {
+  ctx.telegram.sendMessage(process.env.CHAT_ID, 'hello');
+});
+
+bot.launch();
